ba423e0c81c411be5ca4e47c9af440e4f145ed20
Grace <gracecossai@gmail.com>
2024-05-30 00:32:37 +0100

    committed another change

--- src/components/aboutSubBanner/AboutSubBanner.jsx
+++ src/components/aboutSubBanner/AboutSubBanner.jsx
@@ -2,29 +2,38 @@ import React from "react";
 import CountUp from "react-countup";
 import { FaFaceGrinBeam, FaPeopleGroup } from "react-icons/fa6";
 import { FaDatabase } from "react-icons/fa";
+import useScreenSize from "../useScreenSize";
 
 const AboutSubBanner = () => {
+  const { isMobile, isTablet } = useScreenSize();
+
   const counter = [
     {
       icon: <FaFaceGrinBeam />,
-      counts: <CountUp start={0} end={3812} duration={200} />,
+      counts: <CountUp start={0} end={3812} duration={2} />,
       title: "Happy Customers",
     },
     {
       icon: <FaPeopleGroup />,
-      counts: <CountUp start={0} end={100} duration={200} />,
+      counts: <CountUp start={0} end={100} duration={2} />,
       title: "Custom Products",
     },
     {
       icon: <FaDatabase />,
-      counts: <CountUp start={0} end={10} duration={250} />,
+      counts: <CountUp start={0} end={10} duration={2.5} />,
       title: "Branches",
     },
   ];
 
   return (
     <div className='h-[20.3rem] w-full bg-[url("../../../public/images/banner3.jpg")] bg-cover bg-center'>
-      <div className="flex justify-between items-center px-16 h-full w-full bg-darkShade relative">
+      <div
+        className={`${
+          isMobile || isTablet
+            ? "grid grid-cols-2 gap-4 p-4"
+            : "flex justify-between items-center px-16"
+        } h-full w-full bg-darkShade relative`}
+      >
         {counter.map((count, index) => (
           <div key={index} className="text-2xl text-light font-semibold">
             <p className="text-4xl">{count.icon}</p>
@@ -38,3 +47,44 @@ const AboutSubBanner = () => {
 };
 
 export default AboutSubBanner;
+
+// import React from "react";
+// import CountUp from "react-countup";
+// import { FaFaceGrinBeam, FaPeopleGroup } from "react-icons/fa6";
+// import { FaDatabase } from "react-icons/fa";
+
+// const AboutSubBanner = () => {
+//   const counter = [
+//     {
+//       icon: <FaFaceGrinBeam />,
+//       counts: <CountUp start={0} end={3812} duration={200} />,
+//       title: "Happy Customers",
+//     },
+//     {
+//       icon: <FaPeopleGroup />,
+//       counts: <CountUp start={0} end={100} duration={200} />,
+//       title: "Custom Products",
+//     },
+//     {
+//       icon: <FaDatabase />,
+//       counts: <CountUp start={0} end={10} duration={250} />,
+//       title: "Branches",
+//     },
+//   ];
+
+//   return (
+//     <div className='h-[20.3rem] w-full bg-[url("../../../public/images/banner3.jpg")] bg-cover bg-center'>
+//       <div className="flex justify-between items-center px-16 h-full w-full bg-darkShade relative">
+//         {counter.map((count, index) => (
+//           <div key={index} className="text-2xl text-light font-semibold">
+//             <p className="text-4xl">{count.icon}</p>
+//             <p className="text-3xl">{count.counts}</p>
+//             <p>{count.title}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AboutSubBanner;
